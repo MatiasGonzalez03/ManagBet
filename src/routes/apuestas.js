d970ef7f34abc9404de3caa011e8084fcb5209d0
santibusnelli <santibusnelli57@gmail.com>
2023-08-18 03:24:21 -0300

    get y post de apuestas en funcionamiento

--- src/routes/apuestas.js
+++ src/routes/apuestas.js
@@ -7,11 +7,30 @@ router.get('/add', (req, res) => {
     res.render('apuestas/add');
 });
 
+router.post('/add', async(req, res) => {
+    const { dinero, cuota, estado, stake, pais, competicion, partido, pronostico, usuario_id } = req.body;
+    const newApuesta = {
+        dinero,
+        cuota,
+        estado,
+        stake,
+        pais,
+        competicion,
+        partido,
+        pronostico,
+        usuario_id
+    };
+    await pool.query('INSERT INTO apuestas set ?', [newApuesta]);
+    //req.flash('success', 'Apuesta creada satisfactoriamente');
+    //res.redirect('/apuestas');
+    res.send('received');
+});
+
+
 
 router.get('/', async(req, res) => {
     const apuestas = await pool.query('SELECT * FROM apuestas');
-    console.log(apuestas);
-    res.send('apuestas:')
+    res.render('apuestas/list', { apuestas });
 });
 
 
